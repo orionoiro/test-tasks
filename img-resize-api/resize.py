87a9528928a178bc0cb8f3b6caa2cf08dccc285e
Kirill Bergman <k.bergman@protonmail.com>
2020-10-15 17:35:19 +0300

    update

--- img-resize-api/resize.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-
-def resize(width: int, height: int, file):
-    im = Image.open(file)
-    im.resize((width, height))
-    print('job added')
-    im.save('new_image', format=im.format)
