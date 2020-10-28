da915446b1d89f76dc0e44dbf7b5e4f10defc07c
Kirill Bergman <k.bergman@protonmail.com>
2020-10-28 18:00:38 +0300

    almost good

--- img_resize_fastapi/main.py
+++ img_resize_fastapi/main.py
@@ -1,3 +1,5 @@
+from base64 import b64encode
+
 from fastapi import FastAPI, File, UploadFile
 
 from tasks import resize
@@ -6,15 +8,17 @@ app = FastAPI()
 
 
 @app.post('/resize/')
-def set_task(width: int, height: int, file: UploadFile = File(...)) -> dict:
+async def set_task(width: int, height: int, file: UploadFile = File(...)) -> dict:
     if file.content_type not in ('image/jpeg', 'image/png'):
         raise TypeError
     elif not 1 <= height <= 9999:
         raise ValueError('height value should be between 1 and 9999')
     elif not 1 <= width <= 9999:
         raise ValueError('width value should be between 1 and 9999')
-    # change file object to bytes
-    task = resize.delay(width, height, file.file)
+
+    data_bytes = file.file._file.read()  # bytes data of an image
+    b64 = str(b64encode(data_bytes))
+    task = resize.delay(width, height, b64)
     return task
 
 
--- img_resize_fastapi/tasks.py
+++ img_resize_fastapi/tasks.py
@@ -1,21 +1,30 @@
+from base64 import b64encode, b64decode
+from io import BytesIO
+from uuid import uuid4
+
 from PIL import Image
 from celery import Celery
-from uuid import uuid4
 
 app = Celery('tasks', backend='redis://localhost', broker='redis://localhost')
 
-@app.task
-def resize(width: int, height: int, file):
-    try:
-        id = str(uuid4())
-        im = Image.open(file)
-        img_type = im.format
-        im = im.resize((width, height))
-        im.save(id, format=img_type)
-        return id
-    except:
-        raise Exception
 
 @app.task
-def add(x: int, y: int):
-    return x + y
+def resize(width: int, height: int, b64: str):
+    try:
+        data = b64decode(b64)
+        bytes_io = BytesIO(data)
+        uid = str(uuid4())
+        im = Image.open(bytes_io)
+        img_type = im.format
+        im = im.resize((width, height))
+        im.save(uid, format=img_type)
+        return uid
+    except ValueError:
+        print('Error')
+
+if __name__ == '__main__':
+    data = open('/home/dude/Downloads/images/90935.jpg', 'rb')
+    data = data.read()
+    encoded = b64encode(data)
+    b64d = b64decode(data)
+    resize.delay(30, 30, b64d)
